66ffaf0253d43316382215de6a73d3255ae85f44
gamesms <61249403+gamesms@users.noreply.github.com>
2024-02-22 03:51:50 +0300

    Create gamesms1.rb

---
+++ gamesms1.rb
@@ -0,0 +1,10 @@
+class Gamesms1 < Formula
+  desc "A command-line tool for managing SMS messages in a game"
+  homepage "https://example.com/gamesms1"
+  url "https://example.com/gamesms1-1.0.tar.gz"
+  sha256 "<sha256 checksum>"
+  
+  def install
+    bin.install "gamesms1"
+  end
+end
